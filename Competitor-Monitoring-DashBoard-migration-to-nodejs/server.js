ce36b74f2ca82588e5d422962a07c97619c8f967
permanentseoteam-dev <permanentseoteam@gmail.com>
2026-09-21 19:17:45 +0500

    Update server.js

--- Competitor-Monitoring-DashBoard-migration-to-nodejs/server.js
+++ Competitor-Monitoring-DashBoard-migration-to-nodejs/server.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const { webcrypto } = require("crypto");
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
 const { createApp } = require("./src/app");
 
 const port = Number(process.env.PORT) || 3000;
